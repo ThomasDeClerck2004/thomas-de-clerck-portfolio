ec8f21049fb379e8a948fe996331092c75d7b2e7
Thomas De Clerck <thomas.de.clerck4@gmail.com>
2026-05-18 13:54:54 +0200

    added lazy loading to all components

--- src/pages/indexPage.jsx
+++ src/pages/indexPage.jsx
@@ -1,16 +1,28 @@
-import { Hero, About, Work, Contact, Experience, Skills, Hobbies, Internship } from "../components";
+import { lazy, Suspense } from "react";
+import { Hero } from "../components";
+
+const About = lazy(() => import("../components/about"));
+const Hobbies = lazy(() => import("../components/hobbies"));
+const Skills = lazy(() => import("../components/skills"));
+const Experience = lazy(() => import("../components/experience"));
+const Work = lazy(() => import("../components/work"));
+const Internship = lazy(() => import("../components/internship"));
+const Contact = lazy(() => import("../components/contact"));
 
 export default function IndexPage() {
     return (
         <div className="relative z-0">
             <Hero />
-            <About />
-            <Hobbies />
-            <Skills />
-            <Experience />
-            <Work />
-            <Internship />
-            <Contact />
+
+            <Suspense fallback={null}>
+                <About />
+                <Hobbies />
+                <Skills />
+                <Experience />
+                <Work />
+                <Internship />                
+                <Contact /> 
+            </Suspense>
         </div>
     );
 }
